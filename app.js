ab46d86dd2fab9e5ac881e40bf2221bf1ca9fc03
Jonathan Herman <jdh11235@gmail.com>
2015-01-06 12:56:58 -0600

    js getFile

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 /* Copyright (c) 2015 Jonathan Herman. MIT License. */
 /* Web Experiments */
 /* https://github.com/jdh11235/web-experiments */
+
+'use strict';
+
+
+var linksPath = '@';
+var linksContainer = document.getElementById('links');
+
+
+function getFile(url) {
+	var request = new XMLHttpRequest();
+
+	request.open('get', url, true);
+	request.send();
+
+	return request;
+}
+
+function listFolder(path) {
+	var file = getFile(path);
+	var text = file.response;
+	var list = [];
+
+	//do some regex to enumerate filepaths from text into list
+
+	return list;
+}
+
+
+//listFolder(linksPath);
