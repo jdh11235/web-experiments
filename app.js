1063435258133fb9f156bcbaac37b5186c8e3610
Jonathan Herman <jdh11235@gmail.com>
2015-01-06 13:45:20 -0600

    linkTitle, String.toTitleCase, String.capitalize

--- app.js
+++ app.js
@@ -5,9 +5,36 @@
 'use strict';
 
 
+String.prototype.capitalize = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+};
+
+String.prototype.toTitleCase = function() {
+	var words = this.split(' ');
+	var compiledString = '';
+
+	for (var i = 0; i < words.length; i++) {
+		compiledString += (' ' + words[i].capitalize());
+	}
+
+	compiledString = compiledString.substring(1);
+
+	return compiledString;
+};
+
+
 var linksPath = '@';
 var linksContainer = document.getElementById('links');
 
+
+function linkTitle(link) {
+	var title = link.split('/').pop();
+
+	title = title.replace(/-/gi, ' ');
+	title =	title.toTitleCase();
+
+	return title;
+}
 
 function getFile(url) {
 	var request = new XMLHttpRequest();
@@ -28,5 +55,19 @@ function listFolder(path) {
 	return list;
 }
 
+function writeLinks(list) {
+	for (var link in list) {
+		var a = document.createElement('a');
 
-//listFolder(linksPath);
+		a.href = link;
+		a.innerHTML = linkTitle(link);
+
+		linksContainer.appendChild(a);
+	}
+}
+
+
+writeLinks(listFolder(linksPath));
+
+// linkTitle('http://jdh11235.github.io/web-experiments/@/inline-autocomplete');
+// => "Inline Autocomplete"
